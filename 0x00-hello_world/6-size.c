6628bdd948a142dbd528163ce875fe2184a03f73
kumprep <kumatsup1@gmail.com>
2022-12-12 14:11:04 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Return: Always 0
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	print("Size of a char: %i byte(s)\n", sizeof(char));
-	print("Size of an int: %i byte(s)\n", sizeof(int));
-	print("Size of a long long int: %i byte(s)\n", sizeof(long int));
-	print("Size of a float: %i byte(s)\n", sizeof(float));
+	print("Size of a char: %zu byte(s)\n", sizeof(char));
+	print("Size of an int: %zu byte(s)\n", sizeof(int));
+	print("Size of a long long int: %zu byte(s)\n", sizeof(long int));
+	print("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	print("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
